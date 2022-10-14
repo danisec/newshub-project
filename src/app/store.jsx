9e4538dc837f013f21c85ad13ce016207e786e12
syeikhanritzmy <syeikhanritzmy@gmail.com>
2022-10-14 15:50:07 +0800

    add programming Reducer

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import homeReducer from '../features/home/homeSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import homeReducer from '../features/home/homeSlice';
+import programmingReducer from '../features/programming/programmingSlice';
 
 export const store = configureStore({
   reducer: {
     home: homeReducer,
+    programming: programmingReducer,
   },
-})
+});
