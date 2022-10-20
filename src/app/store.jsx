6db43494493d6f5c379b1036e8d28fa37038d551
syeikhanritzmy <syeikhanritzmy@gmail.com>
2022-10-21 03:07:20 +0800

    create reducer for covid-19

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,14 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
-import homeReducer from '../features/home/homeSlice'
-import programmingReducer from '../features/programming/programmingSlice'
-import savedReducer from '../features/saved/savedSlice'
-import searchReducer from '../features/search/searchSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import homeReducer from '../features/home/homeSlice';
+import programmingReducer from '../features/programming/programmingSlice';
+import covidReducer from '../features/covid/covidSlice';
+import savedReducer from '../features/saved/savedSlice';
+import searchReducer from '../features/search/searchSlice';
 
 export const store = configureStore({
   reducer: {
     home: homeReducer,
     programming: programmingReducer,
+    covid: covidReducer,
     saved: savedReducer,
     search: searchReducer,
   },
-})
+});
